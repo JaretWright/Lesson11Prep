791285e18a3944962f578e519d9fde5593075875
JaretWright <hkkA223>
2017-06-09 10:41:08 -0400

    Changed the saving of images and added 404 error handling

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php
+    require_once ('header.php');
+    echo '<main class="container">
+            <h1>What were you thinking?  That link is broken</h1>
+            <img height="400" src="images/404%20surpise.jpg">
+        </main>';
+    require_once ('footer.php');
+?>
